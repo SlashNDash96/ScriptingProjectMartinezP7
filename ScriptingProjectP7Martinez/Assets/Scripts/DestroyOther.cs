5d9867e8f776a54885878cf81670b8d330429cfb
Jayden Martinez <724583@student.dusd.net>
2024-02-21 14:19:03 -0800

    DESTROY
    destroy all cubes

---
+++ ScriptingProjectP7Martinez/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+
+    public GameObject other;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Destroy(other);
+        }
+    }
+}
