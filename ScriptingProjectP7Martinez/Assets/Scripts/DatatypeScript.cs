5a996b7d4f59e6016598fbee686d66d7434d2cf9
Jayden Martinez <724583@student.dusd.net>
2024-03-20 15:28:47 -0700

    DATA
    data types or somethin' idk

---
+++ ScriptingProjectP7Martinez/Assets/Scripts/DatatypeScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class DatatypeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Value type variable
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+
+        //Reference type variable
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
